9ed4cadf125afbae048caa362fb622eaa3de01ee
AnuragJha003 <ajha8931@gmail.com>
2024-04-18 13:00:04 +0530

    Final Fixes made to the Image Gemini Model(problem was in routing)

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
+import React from 'react';
 
 const Home = () => {
   return (
-    <div className='gif-container' style={{
-      marginTop:'120px',
-    }}>
-      <img src="https://cdn.dribbble.com/users/214929/screenshots/4967879/media/cfc4a40efd67ae4810a0975a738d2145.gif" className='home-background'
-      style={{
-        width:"100vw",
-        height:"100vh-150px"
-      }} />
+    <div className='gif-container' style={{ marginTop: '120px', position: 'relative' }}>
+      <img
+        src="https://cdn.dribbble.com/users/214929/screenshots/4967879/media/cfc4a40efd67ae4810a0975a738d2145.gif"
+        className='home-background'
+        style={{ width: "100vw", height: "calc(100vh - 150px)" }}
+        alt="background"
+      />
+      <div className="text-overlay" style={{
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        textAlign: 'center',
+        color: 'white',
+        fontSize: '24px',
+        fontWeight: 'bold',
+        textShadow: '0 0 10px white, 0 0 20px white, 0 0 30px white',
+      }}>
+        GDSC JU Frontend + Backend Route Hitting with Gemini API used
+      </div>
     </div>
   );
 }
 
-export default Home
+export default Home;
